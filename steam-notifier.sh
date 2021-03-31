63dd7de27434f04c46ee6f63d5ef9e8ac1879901
Fabrizio Cirelli <cirelli94@gmail.com>
2021-03-31 10:44:18 +0200

    Removed command substitution with apostrophe Error handling Quoted expansions Removed useless xargs

--- steam-notifier.sh
+++ steam-notifier.sh
@@ -5,35 +5,40 @@
 if ! command -v steamctl &> /dev/null
 then
     echo "steamctl could not be found"
-    echo "install with `pip install steamctl`"
+    echo "install with 'pip install steamctl'"
     echo "more here: https://github.com/ValvePython/steamctl"
     exit 1
 fi
 
 CONFIG="$HOME/.config/steam-notifier"
 
-for GAME in $CONFIG/* ; do
-	APPID=$(basename $GAME )
-	steamctl -l quiet apps product_info $APPID > $GAME.json
-	LAST=$( cat $GAME.json | jq .depots.branches.public.timeupdated | xargs echo )
-	GAME_NAME=$( cat $GAME.json | jq .common.name )
+for GAME in "$CONFIG"/* ; do
+	APPID=$(basename "$GAME" )
 
-	PREVIOUS=$(<$GAME)
+	steamctl -l quiet apps product_info "$APPID" > "$GAME.json" || {
+		echo "Failed steamctl with GAME: [$GAME] and APPID: [$APPID]" 
+		rm "$GAME.json"
+		continue 
+	}
+
+	LAST=$( jq -r .depots.branches.public.timeupdated < "$GAME.json" )
+	GAME_NAME=$( jq -r .common.name < "$GAME.json" )
+
+	PREVIOUS=$(<"$GAME")
 	if [[ -z $PREVIOUS ]]; then
 		PREVIOUS=0
 	fi
 
-	if (( (( $LAST - $PREVIOUS )) > 0 )); then
-		WHEN=$( echo $LAST | xargs -I % sh -c 'date -d @%')
+	if (( (( LAST - PREVIOUS )) > 0 )); then
+		WHEN=$( echo "$LAST" | xargs -I % sh -c 'date -d @%')
 
 		notify-send "$GAME_NAME got an update on $WHEN!" --urgency=critical
-		echo "$GAME_NAME got an update on $WHEN!" --urgency=critical
+		echo "$GAME_NAME got an update on $WHEN!"
 
-		echo $LAST > $GAME
+		echo "$LAST" > "$GAME"
 	else
 		echo "nothing new for $GAME_NAME :("
 	fi
 
-	rm $GAME.json
+	rm "$GAME.json"
 done
-
